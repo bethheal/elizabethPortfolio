7bd294cc62d63c38933ce50e4787cf5b8124fd14
BethTawiah <elizabeth.tawiah@amalitech.org>
2025-08-08 13:13:36 +0000

    remove router dom not in use

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import RootLayout from "./components/RootLayout";
+import { Router } from "react-router";
+// import RootLayout from "./components/RootLayout";
 
 function App() {
   return (
     <Router>
-      <RootLayout />
+      <RootLayout/>
     </Router>
   );
 }
