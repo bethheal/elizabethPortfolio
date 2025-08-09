ea0499a7c53e1f5842f69445e1402ce95c27943e
BethTawiah <elizabeth.tawiah@amalitech.org>
2025-08-09 14:56:04 +0000

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import RootLayout from "./components/RootLayout";
-import { BrowserRouter as Router } from "react-router-dom";
 // import RootLayout from "./components/RootLayout";
 
 function App() {
   return (
-    <Router>
       <RootLayout/>
-    </Router>
   );
 }
 
